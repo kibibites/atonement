d4affb71dd195d9364ae4a151a5beb1e1aefb987
nora <nora@norain.icu>
2025-05-16 21:49:32 -0700

    feat(app): add typography and colors to tailwind

--- packages/app/src/Home.tsx
+++ packages/app/src/Home.tsx
@@ -1,7 +1,7 @@
 import type { Component } from 'solid-js';
 
 const Home: Component = () => (
-  <h1 class="text-2xl font-bold">Hello World</h1>
+  <h1 class="text-2xl font-bold text-safety-red">Hello World</h1>
 );
 
 export default Home;
