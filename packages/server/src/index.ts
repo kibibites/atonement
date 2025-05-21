3712b87c287ba0315d76bd541761d95d9fe62e6f
nora <nora@norain.icu>
2025-05-20 17:52:02 -0700

    feat(server): add auth middleware to hono

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,12 +1,13 @@
 import { Hono } from 'hono';
 import { serve } from '@hono/node-server';
+import auth from './auth';
 
 const app = new Hono();
 
+app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
 app.get('/', (c) => c.json({ success: true }));
 
 const server = serve(app);
-
 console.log('http://localhost:3000');
 
 process.on("SIGINT", () => {
